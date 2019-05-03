dabf1279830b8e1fbff231c311bc39476c4ee4a0
Lakhandutt <lakhandutta23@gmail.com>
2019-05-03 13:03:39 +0530

    1 file modified

--- AsyncronousJavascript/Promises.js
+++ AsyncronousJavascript/Promises.js
@@ -1,15 +1,51 @@
 //creating promises
-const promiseObject=new Promise((resolve,reject)=>{
+const promiseObject = new Promise((resolve, reject) => {
   //kick of some async work
   //...
-  setTimeout(()=>{
+  setTimeout(() => {
     //resolve(1); // State Diagram= pending=>resolve or fulfilled
-   reject(new Error('Error Occured'))//  State Diagram= pending=>rejected
+    reject(new Error('Error Occured'))//  State Diagram= pending=>rejected
 
-  },2000)
+  }, 2000)
 });
 
 //consuming priomises
 promiseObject
-.then(result=>console.log('Result',result))
-.catch(err=>console.log('Error',err.message));
+  .then(result => console.log('Result', result))
+  .catch(err => console.log('Error', err.message));
+
+
+
+//callback Hell solution using promises chaining
+console.log("Before");
+
+getuser(1).
+  then(res => {
+    console.log(res);
+    return getRepositories(res.name);
+  })
+  .then(response => console.log(response)).
+  catch(err => console.log(err.message));
+
+console.log("after");
+
+function getuser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("reading a user from database");
+      resolve({ id: id, name: "lakhan" })
+    }, 2000)
+  });
+
+}
+
+function getRepositories(Name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("reading repos from database");
+      resolve(["repo1", "repo2", "repo3"])
+    }, 2000)
+
+  });
+
+}
